b3a00a539ccfa14dbc105caaf69c793eda4b9b70
JACKSOH <boonkeatjacksoh@gmail.com>
2018-11-23 03:57:35 +0800

    Entity opened 1

--- src/Entity/Invoice.java
+++ src/Entity/Invoice.java
@@ -15,6 +15,71 @@ public class Invoice {
     private String orderID;
     private String paymentStatus;
     private String invoiceAmount;
-    private String incoiceDate;
+    private String invoiceDate;
+
+    @Override
+    public String toString() {
+        return "Invoice{" + "order=" + order + ", invoiceNo=" + invoiceNo + ", orderID=" + orderID + ", paymentStatus=" + paymentStatus + ", invoiceAmount=" + invoiceAmount + ", incoiceDate=" + invoiceDate + '}';
+    }
+
+    public Invoice() {
+    }
+
+    public Invoice(Order order, String invoiceNo, String orderID, String paymentStatus, String invoiceAmount, String invoiceDate) {
+        this.order = order;
+        this.invoiceNo = invoiceNo;
+        this.orderID = orderID;
+        this.paymentStatus = paymentStatus;
+        this.invoiceAmount = invoiceAmount;
+        this.invoiceDate = invoiceDate;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public String getInvoiceNo() {
+        return invoiceNo;
+    }
+
+    public void setInvoiceNo(String invoiceNo) {
+        this.invoiceNo = invoiceNo;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getInvoiceAmount() {
+        return invoiceAmount;
+    }
+
+    public void setInvoiceAmount(String invoiceAmount) {
+        this.invoiceAmount = invoiceAmount;
+    }
+
+    public String getIncoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setIncoiceDate(String incoiceDate) {
+        this.invoiceDate = incoiceDate;
+    }
     
 }
