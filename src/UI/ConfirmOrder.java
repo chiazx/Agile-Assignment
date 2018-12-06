c20481803b9629b01556ffede8bc0f0e420f0052
JACKSOH <boonkeatjacksoh@gmail.com>
2018-12-06 22:31:33 +0800

    sprint 2 done [#161861525]

--- src/UI/ConfirmOrder.java
+++ src/UI/ConfirmOrder.java
@@ -438,7 +438,7 @@ public class ConfirmOrder extends javax.swing.JFrame {
     }//GEN-LAST:event_rbDeliveryActionPerformed
 
     private void CancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CancelActionPerformed
-    System.out.print("cancel");
+    System.out.print("cancel1");
         this.setVisible(false);
     }//GEN-LAST:event_CancelActionPerformed
 
